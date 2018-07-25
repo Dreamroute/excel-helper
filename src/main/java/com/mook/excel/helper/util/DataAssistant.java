9e71a4ded533395cd101cba1b0fe1746a7700f15
w.dehai <342252328@qq.com>
2018-07-25 15:56:45 +0800

    fix: 空值设置默认值

--- src/main/java/com/mook/excel/helper/util/DataAssistant.java
+++ src/main/java/com/mook/excel/helper/util/DataAssistant.java
@@ -5,7 +5,11 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Objects;
 
+import org.apache.poi.ss.usermodel.CellType;
+
+import com.mook.excel.helper.annotation.Column;
 import com.mook.excel.helper.cache.CacheFactory;
 import com.mook.excel.helper.exception.ExcelHelperException;
 
@@ -25,7 +29,19 @@ public class DataAssistant {
             Object data = dataIterator.next();
             for (Field field : fields) {
                 try {
-                    rowData.add(field.get(data));
+                    Object value = field.get(data);
+                    // when cell is null, pre process it, avoid NPE.
+                    if (Objects.isNull(value)) {
+                        CellType ct = field.getAnnotation(Column.class).cellType();
+                        if (ct == CellType.STRING) {
+                            value = "";
+                        } else if (ct == CellType.NUMERIC) {
+                            value = 0;
+                        } else if (ct == CellType.BOOLEAN) {
+                            value = false;
+                        }
+                    }
+                    rowData.add(value);
                 } catch (IllegalArgumentException | IllegalAccessException e) {
                     throw new ExcelHelperException(e);
                 }
