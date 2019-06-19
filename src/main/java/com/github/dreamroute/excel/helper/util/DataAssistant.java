3de2e6da28869058c8a7495d91517da2937d2a01
niechen <634096606@qq.com>
2019-06-19 18:11:53 +0800

    fix: 增加对象属性非空判定

--- src/main/java/com/github/dreamroute/excel/helper/util/DataAssistant.java
+++ src/main/java/com/github/dreamroute/excel/helper/util/DataAssistant.java
@@ -115,7 +115,9 @@ public class DataAssistant {
                 }
                 columnNumber++;
             }
-            data.add(domain);
+            if (!isAllFieldNull(domain)) {
+                data.add(domain);
+            }
             lineNumber++;
         }
         if (StringUtils.isNotBlank(errorMessage)) {
@@ -168,6 +170,34 @@ public class DataAssistant {
             }
         }
         return value;
+    }
+
+    /**
+     *     判断该对象是否: 返回ture表示所有属性为null  返回false表示不是所有属性都是null
+     */
+    public static boolean isAllFieldNull(Object obj){
+        // 得到类对象
+        Class stuCla = obj.getClass();
+        //得到属性集合
+        Field[] fs = stuCla.getDeclaredFields();
+        boolean flag = true;
+        try {
+            //遍历属性
+            for (Field f : fs) {
+                // 设置属性是可以访问的(私有的也可以)
+                f.setAccessible(true);
+                // 得到此属性的值
+                Object val = f.get(obj);
+                if(val!=null) {
+                    //只要有1个属性不为空,那么就不是所有的属性值都为空
+                    flag = false;
+                    break;
+                }
+            }
+        }catch (Exception e) {
+            flag=false;
+        }
+        return flag;
     }
 
     public static void main(String[] args) {
